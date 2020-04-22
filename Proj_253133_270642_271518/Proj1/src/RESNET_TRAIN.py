3809c0873b5bbe6cfba12064916fd7c58b4d9a0d
CeliaBenquet <32598028+CeliaBenquet@users.noreply.github.com>
2020-04-22 12:10:04 +0200

    clean imgs

--- Proj_253133_270642_271518/Proj1/src/RESNET_TRAIN.py
+++ Proj_253133_270642_271518/Proj1/src/RESNET_TRAIN.py
@@ -2,29 +2,30 @@ import subprocess
 import os
 
 
-PYTHON_INTER = 'python' #change accordingly to your environment 
+PYTHON_INTER = 'python-3-7' #change accordingly to your environment
 
-NB_RESIDUAL_BLOCKS_RANGE = [1, 2, 3, 4, 5]
-NB_CHANNELS_RANGE = [1, 2, 4, 8, 16]
-KERNEL_SIZE_RANGE = [3, 5, 7]
+#NB_RESIDUAL_BLOCKS_RANGE = [1, 2, 3, 4, 5]
+#NB_CHANNELS_RANGE = [1, 2, 4, 8, 16]
+KERNEL_SIZE_RANGE = [3, 5]
 
-RESIDUAL = True
-BN = True
+#RESIDUAL = True
+BN = [True, False]
+DROPOUT = [True, False]
 
 FNULL = open(os.devnull, 'w')
 
-nb_eval = len(NB_RESIDUAL_BLOCKS_RANGE) * len(NB_CHANNELS_RANGE) * len(KERNEL_SIZE_RANGE)
+nb_eval = len(BN) * len(DROPOUT) * len(KERNEL_SIZE_RANGE)
 eval_ = 0
 print('* Number of evaluations : {}'.format(nb_eval))
-for i in NB_RESIDUAL_BLOCKS_RANGE:
-    for j in NB_CHANNELS_RANGE:
-        for k in KERNEL_SIZE_RANGE:
+for i in KERNEL_SIZE_RANGE:
+    for j in BN:
+        for k in DROPOUT:
             eval_ += 1
-            params = '--architecture resnet --save_fig --nb_residual_blocks {0} --nb_channels {1} --kernel_size {2} --force_axis --datasize 350'.format(i, j, k)
-            if BN :
+            params = '--architecture lenet --save_fig --kernel_size {} --force_axis --datasize 350'.format(i)
+            if j: #BN
                 params += ' --bn'
-            if RESIDUAL :
-                params += ' --residual'
+            if k: #dropout
+                params += ' --dropout'
             print(params)
             subprocess.call('{0} dev.py {1}'.format(PYTHON_INTER, params), shell=True, stdout=FNULL)
             print('** Evaluation #{} Done ({:.1f}% overall)'.format(eval_, eval_*100/nb_eval))
