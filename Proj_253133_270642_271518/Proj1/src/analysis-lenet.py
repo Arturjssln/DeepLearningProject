28254063d248c0768e9487e8484f791a6124bf6e
Arturjssln <arturjesslen@gmail.com>
2020-04-22 14:29:37 +0200

    lenet and resnet separation

---
+++ Proj_253133_270642_271518/Proj1/src/analysis-lenet.py
@@ -0,0 +1,27 @@
+import subprocess
+import os
+
+
+PYTHON_INTER = 'python' #change accordingly to your environment
+
+KERNEL_SIZE_RANGE = [3, 5]
+BN = [True, False]
+DROPOUT = [True, False]
+
+FNULL = open(os.devnull, 'w')
+
+nb_eval = len(BN) * len(DROPOUT) * len(KERNEL_SIZE_RANGE)
+eval_ = 0
+print('* Number of evaluations : {}'.format(nb_eval))
+for i in KERNEL_SIZE_RANGE:
+    for j in BN:
+        for k in DROPOUT:
+            eval_ += 1
+            params = '--architecture lenet --save_fig --kernel_size {} --force_axis --datasize 350'.format(i)
+            if j: #BN
+                params += ' --bn'
+            if k: #dropout
+                params += ' --dropout'
+            print(params)
+            subprocess.call('{0} dev.py {1}'.format(PYTHON_INTER, params), shell=True, stdout=FNULL)
+            print('** Evaluation #{} Done ({:.1f}% overall)'.format(eval_, eval_*100/nb_eval))
